4bd8fea1c3b26fb84fbe22cb97c0982b3ff769e4
Erick Pinos <erickpinos@mit.edu>
2019-03-16 00:58:44 -0400

    modularized tests

--- main/main.ino
+++ main/main.ino
@@ -3,49 +3,23 @@
 // How many leds in your strip?
 #define NUM_LEDS 240
 
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
 // For led chips like Neopixels, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
 // ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
 #define DATA_PIN 3
 #define CLOCK_PIN 13
 
-// Define the array of leds
-CRGB leds[NUM_LEDS];
-
-
 void setup() {
-  // Uncomment/edit one of the following lines for your leds arrangement.
-  // FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  // FastLED.addLeds<APA104, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<UCS1903B, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<GW6205, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<GW6205_400, DATA_PIN, RGB>(leds, NUM_LEDS);
-  
-  // FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<SM16716, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<LPD8806, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<P9813, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<APA102, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<DOTSTAR, RGB>(leds, NUM_LEDS);
-
-  // FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<P9813, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<DOTSTAR, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
 
   resetStream();
 
   Serial.begin(9600); // set the baud rate
   Serial.println("Ready"); // print "Ready" once
+
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
@@ -55,29 +29,44 @@ void loop() {
   if(Serial.available()){ // only send data back if data has been sent
     char inByte = Serial.read(); // read the incoming data
     Serial.println(inByte); // send the data back in a new line so that it is not all one long line
-    stream(1);
-    if(inByte == '0') {
-//      digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-//      delay(1000);                       // wait for a second
 
-      // Now turn the LED off, then pause
-//      leds[0] = CRGB::Black;
-//      FastLED.show();
-//      delay(500);
 
-    }
-
-    if(inByte == '1'){
-      // Turn the LED on, then pause
-//      leds[0] = CRGB::Red;
-//      FastLED.show();
-//      delay(500);
-
-//      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-//      delay(1000);                       // wait for a second
-    }
+    // Pick one
+    testOnboardLight(inByte);
+    //testLEDStripLight(inByte);
+    //stream(1);
   }
   delay(100); // delay for 1/10 of a second
+}
+
+void testOnboardLight(char inByte) {
+  if(inByte == '0') {
+    // Turn the LED off, then pause
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(1000);                       // wait for a second
+  }
+
+  if(inByte == '1'){
+    // Turn the LED on, then pause
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);                       // wait for a second
+   }
+}
+
+void testLEDStripLight(char inByte) {
+  if(inByte == '0') {
+    // Turn the LED off, then pause
+    leds[0] = CRGB::Black;
+    FastLED.show();
+    delay(500);
+  }
+
+  if(inByte == '1'){
+    // Turn the LED on, then pause
+    leds[0] = CRGB::Red;
+    FastLED.show();
+    delay(500);
+  }
 }
 
 void testStream() {
